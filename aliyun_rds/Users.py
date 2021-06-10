98fa0dade9f2e19b0f78f8aeca56141c1f2aaf18
AEight1973 <1015252562@163.com>
2021-06-10 18:30:54 +0800

    Change from Desktop_T70RDMU

--- aliyun_rds/Users.py
+++ aliyun_rds/Users.py
@@ -1,10 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from sqlalchemy import create_engine
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.ini')
+
+db_url = "mysql+pymysql://{username}:{password}@{inner_path}:{port}/users".format(
+    username=config.get('account', 'USERNAME'),
+    password=config.get('account', 'PASSWORD'),
+    inner_path=config.get('db_connect', 'INNER_PATH'),
+    port=config.get('db_connect', 'PORT')
+)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://username:password@inner_path:port/users"
-
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 db = SQLAlchemy(app)
 
 
@@ -13,10 +22,20 @@ class UserInfo(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     authority = db.Column(db.Integer, nullable=False)
+    account = db.Column()
 
 
 class LoginRecord(db.Model):
     __tablename__ = 'loginrecord'
 
     num = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id = db.Column(db.Integer)
+    id = db.Column(db.Integer, db.ForeignKey('userinfo.id'))
+    iplist = db.Column(db.String)
+
+
+class UseRecord(db.Model):
+    __tablename__ = 'loginrecord'
+
+    num = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, db.ForeignKey('userinfo.id'))
+    uselist = db.Column(db.String)
